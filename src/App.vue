f24f7baa5c6f4cacba8ebc9ed6755374c15eb913
ashish <shaotaa@tcd.ie>
2022-09-27 13:53:04 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,157 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <HeaderTab :showAddTaskBtn="showAddTaskBtn" title="Task Traker" @toggle-add-form="toggleForm"/>
+    <div v-show="showAddTaskForm">
+      <AddTask @add-task="addTask" />
+    </div>
+    <TasksComponent
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HeaderTab from './components/Header.vue'
+import AddTask from './components/AddTask.vue'
+
+import TasksComponent from "./components/TasksComponent.vue";
+
+
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+		HeaderTab,
+		TasksComponent,
+		AddTask
+  },
+  methods: {
+		async deleteTask(id){
+
+		if(confirm('Are you Sure ?'))
+		{
+				const res = await fetch(`api/tasks/${id}`,
+				{
+						method: 'DELETE'				
+				})
+				res.status === 200 ? (this.tasks = this.tasks.filter(task => task.id !== id)) : alert('Error deleting the task!')
+		}
+
+  },
+		async toggleReminder(id){
+				const tsk = await this.fetchTask(id) 
+				const updTask = {...tsk, reminder : !tsk.reminder}
+
+				const res = await fetch(`api/tasks/${id}`, {
+						method: 'PUT',
+						headers: {
+								'Content-type': 'application/json'
+						},
+						body: JSON.stringify(updTask)
+
+				})
+
+				const data = await res.json();
+
+				this.tasks = this.tasks.map(task => (task.id === id ?{...task, reminder: data.reminder} : task)
+				)
+		},
+
+		async addTask(newTask){
+				const res = await fetch('api/tasks', {
+				method:'POST', 
+				headers :{
+						'Content-type': 'application/json',
+				},
+
+						'body': JSON.stringify(newTask)
+
+				})
+
+				const data= await res.json()
+
+				this.tasks = [...this.tasks, data];
+		},
+		toggleForm(){
+				this.showAddTaskForm = !this.showAddTaskForm;
+				if(this.showAddTaskForm)
+						this.showAddTaskBtn = true;
+				else
+						this.showAddTaskBtn = false;
+						
+		},
+
+		async fetchTasks()
+		{
+				const res = await fetch('api/tasks')
+				const data = await res.json();
+
+				return data
+		},
+		async fetchTask(id)
+		{
+				const res = await fetch(`api/tasks/${id}`)
+				const data = await res.json()
+				return data
+		}
+  },
+  data (){
+		return {
+		tasks: [],
+		showAddTaskForm: false,
+		showAddTaskBtn:false 
+		}
+  },
+  async created() {
+		this.tasks = await this.fetchTasks()
   }
-}
+
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <CustomButton :text="showAddTaskBtn ? 'Close' : 'Add Task'" 
+	:color="showAddTaskBtn ? 'red' : 'green'" @btn-click="$emit('toggle-add-form')"/>
+	
+  </header>
+</template>
+
+<script>
+import CustomButton from "./CustomButton.vue";
+
+
+export default {
+  name: "HeaderTab",
+  props: {
+    title: String,
+	showAddTaskBtn: Boolean
+  },
+  components: {
+    CustomButton,
+  },
+  emits: ['toggle-add-form']
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
